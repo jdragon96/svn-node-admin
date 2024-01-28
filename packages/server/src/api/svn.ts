17b0a4c692257c47c5a69451dc8dfe1966969ee4
jdgragon96 <tjdwodyd7712@gmail.com>
2024-01-28 19:01:03 +0900

    2024 01 28 update

--- packages/server/src/api/svn.ts
+++ packages/server/src/api/svn.ts
@@ -1,5 +1,16 @@
 import express, { Express, Request, Response, NextFunction } from 'express';
 import * as process from "child_process";
+import * as Model from "../model/models"
+
+const create_default_packet = <T>(flag: boolean, msg: string, body: T) => {
+  var result: Model.response_packet<T> = 
+  {
+    is_success: flag,
+    message: msg,
+    body: body
+  }
+  return result;
+}
 
 export const svn_router = express.Router();
 
@@ -11,73 +22,69 @@ svn_router.use("/", (req: Request, res: Response, next: NextFunction) => {
   next()
 });
 
-interface svn_heartheat_request{
-  query: string
-}
-interface response_packet<T>{
-  is_success: boolean
-  message: string
-  query: T
-}
+
+//! find svn repository root path
+svn_router.get("/root", async (req: Request, res: Response) => {
+  var query: Model.svn_heartheat_request = JSON.parse(JSON.stringify(req.query));
+  var hb_response = create_default_packet<string>(true, "", svn_root_path);
+  res.send(hb_response);
+});
+
+
+//! check status svn process either live or dead.
 svn_router.get("/heartbeat", async (req: Request, res: Response) => {
-  var query: svn_heartheat_request = JSON.parse(JSON.stringify(req.query));
+  var query: Model.svn_heartheat_request = JSON.parse(JSON.stringify(req.query));
   var p = await process.exec(query.query, (err, output) => 
   {
-    var hb_response: response_packet<string> = 
-    {
-      is_success: err ? false : true,
-      message: output,
-      query: output
-    }
+    var hb_response = create_default_packet<string>(err ? true : false, output, output);
     res.send(hb_response);
   });
 });
 
-interface svn_create_request{
-  repository_name: string
-}
+
+//! create new repositoy
 svn_router.get("/create", async (req: Request, res: Response) => {
-  var query: svn_heartheat_request = JSON.parse(JSON.stringify(req.query));
-  var p = await process.exec(query.query, (err, output) => 
+  var query: Model.svn_create_request = JSON.parse(JSON.stringify(req.query));
+  var command = "svnadmin create --fs-type fsfs " + 
+                  svn_root_path + 
+                  "/" + 
+                  query.repository_name;
+
+  var p = await process.exec(command, (err, output) => 
   {
-    var hb_response: response_packet<string> = 
+    var response = create_default_packet<string>(err ? true : false, output, output);
+    
+    // error check
+    if(output.length < 1)
     {
-      is_success: err ? false : true,
-      message: output,
-      query: output
+      response.is_success = true;
+      response.message = "success";
     }
-    res.send(hb_response);
+    else if (output.indexOf("E165002") < 0)
+    {
+      response.is_success = false;
+      response.message = "Already exist repository. please use another name!";
+    }
+
+    res.send(response);
   });
 });
 
-interface svn_list_request{
-}
-interface repository{
-  name: string
-}
-interface svn_list_response{
-  repostiories: repository[]
-}
+
+//! find all repository
 svn_router.get("/list", async (req: Request, res: Response) => {
   var command = "ls " + svn_root_path;
 
   var p = await process.exec(command, (err, output) => 
   {
-    var hb_response: response_packet<string> = 
-    {
-      is_success: err ? false : true,
-      message: output,
-      query: ""
-    }
-    
-    var packet: svn_list_response = 
+    var response = create_default_packet<string>(err ? true : false, output, output);
+    var packet: Model.svn_list_response = 
     {
       repostiories: []
     }
-    
-    console.log(output);
+
+    // delete last word
     var repo_list = output.split("\n");
-    console.log(repo_list);
     for(var repo of repo_list)
     {
       if(repo === "") continue;
@@ -86,7 +93,8 @@ svn_router.get("/list", async (req: Request, res: Response) => {
       });
     }
 
-    hb_response.query = JSON.stringify(packet)
-    res.send(hb_response);
+    // serialization
+    response.body = JSON.stringify(packet)
+    res.send(response);
   });
 });
